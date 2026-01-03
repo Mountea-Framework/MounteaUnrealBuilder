13f21252fe08531719a68e8fe5a1f794a7d761f0
Dominik Morse <aenszguhelem@gmail.com>
2026-01-03 23:24:26 +0100

    Fixing Win version using Mac paths

--- src/main/builder.ts
+++ src/main/builder.ts
@@ -189,7 +189,7 @@ export class BuildExecutor {
         ];
       }
 
-      const spawnCommand = `'${runatPath}'`;
+      const spawnCommand = `"${runatPath}"`;
       
       this.currentProcess = spawn(spawnCommand, args, {
         shell: true,
